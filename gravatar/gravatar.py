4e216a630b5e3e14cc90c136a54389d0bbad1e14
Daker Fernandes Pinheiro <dakerfp@gmail.com>
2015-12-10 10:21:40 -0300

    Add 'about_author' using gravatar API.

--- gravatar/gravatar.py
+++ gravatar/gravatar.py
@@ -7,7 +7,9 @@ makes the variable available within the article's context.
 """
 
 import hashlib
+import json
 import six
+import urllib2
 
 from pelican import signals
 
@@ -19,12 +21,13 @@ def add_gravatar(generator, metadata):
         and 'AUTHOR_EMAIL' in generator.settings.keys():
             metadata['email'] = generator.settings['AUTHOR_EMAIL']
 
-    #then add gravatar url
+    #then add gravatar url and about
     if 'email' in metadata.keys():
-        email_bytes = six.b(metadata['email']).lower()
-        gravatar_url = "https://www.gravatar.com/avatar/" + \
-                        hashlib.md5(email_bytes).hexdigest()
-        metadata["author_gravatar"] = gravatar_url
+        gravatar_url = "http://en.gravatar.com/" + metadata['email'] + ".json"
+        data = json.load(urllib2.urlopen(gravatar_url))
+        metadata['author_gravatar'] = data['entry'][0]['thumbnailUrl']
+        if 'about_author' not in metadata.keys():
+            metadata['about_author'] = data['entry'][0]['aboutMe']
 
 
 def register():
